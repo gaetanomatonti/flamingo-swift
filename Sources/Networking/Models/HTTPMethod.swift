54d65fd2edc47cd4b252d84b222c64b5b1ad241d
Gaetano Matonti <gaetanomatonti@icloud.com>
2023-04-05 20:48:35 +0200

    Add HTTP methods

--- Sources/Networking/Models/HTTPMethod.swift
+++ Sources/Networking/Models/HTTPMethod.swift
@@ -2,4 +2,13 @@
 public enum HTTPMethod: String {
   /// `GET` method.
   case get = "GET"
+  
+  /// `POST` method.
+  case post = "POST"
+  
+  /// `PATCH` method.
+  case patch = "PATCH"
+  
+  /// `DELETE` method.
+  case delete = "DELETE"
 }
